454dceeae9399fb4e65cf278c59a84f3c26f794b
Igor Mucsicska <mucsi_96@yahoo.com>
2020-05-02 23:35:49 +0200

    Use node integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { arch } from "os";
+import React from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <p>Hello from {arch()} architecture</p>
       </header>
     </div>
   );
